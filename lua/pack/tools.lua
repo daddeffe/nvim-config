45c83d8048af6f29b4604a005a4b458f2f2682c4
Davide Frattarolo <daddef@gmail.com>
2026-01-17 13:50:01 +0100

    Disabled autocmd adn opencode binds

--- lua/pack/tools.lua
+++ lua/pack/tools.lua
@@ -249,41 +249,41 @@ vim.g.opencode_opts = {
     },
   },
 }
-vim.keymap.set({ 'n', 'x' }, '<C-a>', function()
-  require('opencode').ask('@this: ', { submit = true })
-end, { desc = 'Ask opencode' })
-vim.keymap.set({ 'n', 'x' }, '<C-x>', function()
-  require('opencode').select()
-end, { desc = 'Execute opencode action…' })
-vim.keymap.set({ 'n', 'x' }, 'ga', function()
-  require('opencode').prompt '@this'
-end, { desc = 'Add to opencode' })
-vim.keymap.set({ 'n', 't' }, '<C-.>', function()
-  require('opencode').toggle()
-end, { desc = 'Toggle opencode' })
-vim.keymap.set('n', '<S-C-u>', function()
-  require('opencode').command 'session.half.page.up'
-end, { desc = 'opencode half page up' })
-vim.keymap.set('n', '<S-C-d>', function()
-  require('opencode').command 'session.half.page.down'
-end, { desc = 'opencode half page down' })
+-- vim.keymap.set({ 'n', 'x' }, '<C-a>', function()
+--   require('opencode').ask('@this: ', { submit = true })
+-- end, { desc = 'Ask opencode' })
+-- vim.keymap.set({ 'n', 'x' }, '<C-x>', function()
+--   require('opencode').select()
+-- end, { desc = 'Execute opencode action…' })
+-- vim.keymap.set({ 'n', 'x' }, 'ga', function()
+--   require('opencode').prompt '@this'
+-- end, { desc = 'Add to opencode' })
+-- vim.keymap.set({ 'n', 't' }, '<C-.>', function()
+--   require('opencode').toggle()
+-- end, { desc = 'Toggle opencode' })
+-- vim.keymap.set('n', '<S-C-u>', function()
+--   require('opencode').command 'session.half.page.up'
+-- end, { desc = 'opencode half page up' })
+-- vim.keymap.set('n', '<S-C-d>', function()
+--   require('opencode').command 'session.half.page.down'
+-- end, { desc = 'opencode half page down' })
 
-vim.keymap.set({ 'n', 'i' }, '<C-k>', function()
-  if vim.fn.mode() == 'i' then
-    vim.cmd 'stopinsert'
-  end
-
-  local line = vim.api.nvim_get_current_line()
-  require('opencode').ask('@this Complete this code/comment: ' .. line, { submit = true })
-end, { desc = 'OpenCode autocomplete from current line' })
-
-vim.keymap.set({ 'n', 'x', 'i' }, '<C-S-k>', function()
-  if vim.fn.mode() == 'i' then
-    vim.cmd 'stopinsert'
-  end
-
-  require('opencode').ask('@this Continue writing this code naturally, providing only the next logical lines without explanation: ', { submit = true })
-end, { desc = 'OpenCode smart completion' })
+-- vim.keymap.set({ 'n', 'i' }, '<C-k>', function()
+--   if vim.fn.mode() == 'i' then
+--     vim.cmd 'stopinsert'
+--   end
+--
+--   local line = vim.api.nvim_get_current_line()
+--   require('opencode').ask('@this Complete this code/comment: ' .. line, { submit = true })
+-- end, { desc = 'OpenCode autocomplete from current line' })
+--
+-- vim.keymap.set({ 'n', 'x', 'i' }, '<C-S-k>', function()
+--   if vim.fn.mode() == 'i' then
+--     vim.cmd 'stopinsert'
+--   end
+--
+--   require('opencode').ask('@this Continue writing this code naturally, providing only the next logical lines without explanation: ', { submit = true })
+-- end, { desc = 'OpenCode smart completion' })
 
 -- Lazygit configuration
 require('telescope').load_extension 'lazygit'
